427dc7b3a234fff1e7bcad73cab2249f7179b440
Vexatori <44672606+Vexatori@users.noreply.github.com>
2019-01-28 19:16:47 +0300

    HW is done

---
+++ AlgorithmHW5/BracketChain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmHW5
+{
+    public static class BracketChain
+    {
+        /// <summary>
+        /// Метод возвращает true, если текущая последовательность скобок правильная, и false, если она неправильная
+        /// </summary>
+        /// <param name="chain"></param>
+        /// <returns></returns>
+        public static bool IsCorrect(string chain)
+        {
+            char[] chainArr = chain.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            for(int i = 0; i < chainArr.Length; i++)
+            {
+                if(chainArr[ i ] == '(' || chainArr[ i ] == '{' || chainArr[ i ] == '[')
+                {
+                    stack.Push(chainArr[ i ]);
+                }
+                else
+                {
+                    if(chainArr[ i ] == ')' && stack.Count != 0 && stack.Peek() == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else if(chainArr[ i ] == '}' && stack.Count != 0 && stack.Peek() == '{')
+                    {
+                        stack.Pop();
+                    }
+                    else if(chainArr[ i ] == ']' && stack.Count != 0 && stack.Peek() == '[')
+                    {
+                        stack.Pop();
+                    }
+                    else if(( chainArr[ i ] == '}' || chainArr[ i ] == ')' || chainArr[ i ] == ']') && stack.Count == 0)
+                    {
+                        stack.Push('e');
+                        break;
+                    }
+                }
+            }
+            if(stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AlgorithmHW5/Postfix.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmHW5
+{
+    public static class Postfix
+    {
+        /// <summary>
+        /// Операторы и их приоритет
+        /// </summary>
+        private static Dictionary<char, int> ExpressionPriority = new Dictionary<char, int>
+        {
+            {'+', 2},
+            {'-', 2},
+            {'*', 3},
+            {'/', 3},
+            {'^', 4}
+        };
+
+        /// <summary>
+        /// Метод возвращает приоритет текущего оператора
+        /// </summary>
+        /// <param name="operation">Проверяемый оператор</param>
+        /// <returns></returns>
+        private static int GetOperationPriority(char operation)
+        {
+            int priority = 0;
+            foreach(var op in ExpressionPriority)
+            {
+                if(operation == op.Key)
+                {
+                    priority = op.Value;
+                }
+            }
+            return priority;
+        }
+
+        /// <summary>
+        /// Метод перестраивает входящую инфиксную запись в постфиксную
+        /// </summary>
+        /// <param name="str">Инфиксная запись выражения</param>
+        /// <returns></returns>
+        public static string RebuildToPostfix(string str)
+        {
+            Stack<(char, int)> op = new Stack<(char, int)>();
+            string expression = string.Concat(str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            char[] temp = expression.ToCharArray();
+            StringBuilder result = new StringBuilder();
+            foreach(char sym in temp)
+            {
+                if(sym >= '0' && sym <= '9')
+                {
+                    result.Append(sym);
+                }
+                if(sym == '(')
+                {
+                    op.Push((sym, 0));
+                }
+                if(sym == ')')
+                {
+                    while(op.Peek().Item1 != '(')
+                    {
+                        result.Append(op.Pop().Item1);
+                    }
+                    if(op.Peek().Item1 == '(')
+                    {
+                        op.Pop();
+                    }
+                }
+                if(ExpressionPriority.ContainsKey(sym))
+                {
+                    if(op.Count != 0)
+                    {
+                        while(op.Count != 0 && op.Peek().Item1 != '(' && op.Peek().Item2 >= GetOperationPriority(sym))
+                        {
+                            result.Append(op.Pop().Item1);
+                        }
+                    }
+                    if(sym == '(')
+                    {
+                        op.Push((sym, 0));
+                    }
+                    op.Push((sym, GetOperationPriority(sym)));
+                }
+            }
+            while(op.Count != 0)
+            {
+                result.Append(op.Pop().Item1);
+            }
+            return result.ToString();
+        }
+    }
+}
